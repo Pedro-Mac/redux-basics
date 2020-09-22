51fe598ff7f51032c92b8985279a660bf53cc095
KozaX <55134615+KozaX@users.noreply.github.com>
2020-09-22 17:30:07 +0200

    refactor if statement

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,26 +2,27 @@ const initialState = {
   counter: 0
 };
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      ...state,
-      counter: state.counter + 1
-    };
-  } else if (action.type === 'DECREMENT') {
-    return {
-      ...state,
-      counter: state.counter - 1
-    };
-  } else if (action.type === 'ADD_VALUE') {
-    return {
-      ...state,
-      counter: state.counter + action.value
-    };
-  } else if (action.type === 'SUBTRACT_VALUE') {
-    return {
-      ...state,
-      counter: state.counter + action.value
-    };
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case 'ADD_VALUE':
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case 'SUBTRACT_VALUE':
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
   }
   return state;
 };
